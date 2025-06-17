7b7d67ab7ccd9b1bcd229236ef8bf25eaa092b26
Thanh <cringeclone@gmail.com>
2025-06-17 18:44:23 +0900

    remove kuromoji

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const express = require('express')
 const cors = require('cors')
 // const { fileURLToPath } =  require('url')
 // const { dirname } = require('path')
-const { runReader, runAnalyzer } = require('./jsonReader.js')
+const { runReader } = require('./jsonReader.js')
 const path = require('path')
 const kuromoji = require('kuromojiFORK')
 
@@ -99,21 +99,7 @@ app.post('/api/search', (req, res) => {
   }
 
   const word = req.body.content
-  if (runReader(word).answer !== 'no result found') {
-    res.json([{ basic_form: '*', runreader: runReader(word)}])
-    return
-  }
-
-  runAnalyzer(word, (token) => {
-    let arr = []
-      token.forEach((item) =>
-        arr.push({
-          basic_form: item.basic_form,
-          runreader: runReader(item.basic_form),
-        })
-      )
-      res.json(arr)
-  })
+  res.json(runReader(word))
 })
 
 app.post('/api/notes', (req, res) => {
